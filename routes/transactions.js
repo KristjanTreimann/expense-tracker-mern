a2a9d2ef4c69eaff19472c31f178b2e303b41c66
Kristjan T <43129577+KristjanTreimann@users.noreply.github.com>
2020-07-10 22:03:50 +0300

    GET,POST,DELETE routes set up

--- routes/transactions.js
+++ routes/transactions.js
@@ -4,7 +4,19 @@ const express = require('express')
 // Use router
 const router = express.Router()
 
-router.get('/', (req, res) => res.send('Hello'))
+// bring in from transactions functions from transactionController
+const {
+  getTransactions,
+  addTransaction,
+  deleteTransaction
+} = require('../controllers/transactionController')
+
+// Anytime we make a request to '/' which represents '/api/v1/transactions' from server.js because its connected to this file
+// Then we can add methods here '.get()'. We make get request to that and we want to call getTransactions functions
+router.route('/').get(getTransactions).post(addTransaction)
+
+// because the delete needs an id
+router.route('/:id').delete(deleteTransaction)
 
 // export router in order to this
 module.exports = router
